83d8a09fa39a94989fd2f2a275e4619302d4b742
diegouu <die.sepulvedah@duocuc.cl>
2025-01-17 11:22:46 -0300

    for

---
+++ for.py
@@ -0,0 +1,11 @@
+nombre = "hola soy pedro"
+contador=0
+print(nombre[0])
+
+for i in nombre:
+   contador+=1
+   if i == "p":
+      print("encontre la p en la pocision ", contador)
+   if i == "y":
+      print("encontre la y")
+      
